e4acdedff06da18dd8b60e11f400e7f88d97c348
mk <mkao006@gmail.com>
2016-07-19 07:08:06 -0700

    added validation and test data creation

--- mnist/mnist_loader.R
+++ mnist/mnist_loader.R
@@ -1,26 +1,63 @@
-## Need to execute the mnist_data_converter.py
+## Optional: This converts the data from .pkl to .csv.
+##
+## system("python mnist_data_converter.py")
+
+## Load the library
 library(data.table)
 library(magrittr)
 library(dplyr)
 
 ## Read the data
 mnist =
-    fread(input = "mnist.csv", stringsAsFactors = FALSE, header = TRUE)
-
+    fread(input = "mnist.csv",
+          stringsAsFactors = FALSE,
+          header = TRUE)
 
 ## Remove the first column which is the row names
 mnist =
     mnist %>%
     select(., -V1)
 
+## Create the training data
 train_data =
     mnist %>%
     subset(., set == "train") %>%
     select(., matches("^[0-9]")) %>%
     as.matrix
 
+## Create the training label
 train_data_label =
     mnist %>%
     subset(., set == "train") %>%
+    select(., label) %>%
+    as.matrix
+
+
+## Create the validation data
+validation_data =
+    mnist %>%
+    subset(., set == "validation") %>%
+    select(., matches("^[0-9]")) %>%
+    as.matrix
+
+## Create the validation label
+validation_data_label =
+    mnist %>%
+    subset(., set == "validation") %>%
+    select(., label) %>%
+    as.matrix
+
+
+## Create the testing data
+test_data =
+    mnist %>%
+    subset(., set == "test") %>%
+    select(., matches("^[0-9]")) %>%
+    as.matrix
+
+## Create the testing label
+test_data_label =
+    mnist %>%
+    subset(., set == "test") %>%
     select(., label) %>%
     as.matrix
