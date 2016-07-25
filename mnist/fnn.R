ebd99fd4817e9bc188fa130b9d79591f1aa204c5
mk <mkao006@gmail.com>
2016-07-24 17:17:34 -0700

    moved training code from train to fnn and also implemented stochastic gradient descent

--- mnist/fnn.R
+++ mnist/fnn.R
@@ -34,4 +34,74 @@ translation_delta_x = function(w){
     t(w)
 }
 
+initialise_weight = function(input_size, output_size){
+    apply(X = matrix(rnorm(input_size * output_size),
+                      nr = input_size,
+                      nc = output_size),
+           MARGIN = 2,
+          FUN = function(x) x/sum(x))
+}
 
+train = function(data,
+                 label,
+                 weights,
+                 maxIter,
+                 tol,
+                 stochastic = TRUE,
+                 sampling_pct = 0.3,
+                 gamma = 1e-3){
+    i = 1
+    c_old = -Inf
+
+    w0 = weights[[1]]
+    w1 = weights[[2]]
+    n = NROW(data)
+
+    n_sample = floor(n * sampling_pct)
+
+    while(i <= maxIter){
+        if(stochastic){
+            index_sample = sample(n, n_sample)
+            train_data = data[index_sample, ]
+            train_data_label = label[index_sample, ]
+        }
+
+
+        ## Forward propagation
+        t1 = translation(train_data, w0)
+        a1 = activation(t1)
+        t2 = translation(a1, w1)
+        a2 = activation(t2)
+
+        ## print(head(a2))
+        ## print(head(train_data_label_bin))
+
+        c = cost(train_data_label, a2)
+        message("Cost: ", c)
+        if((c - c_old) < tol)
+            break
+
+        ## Calculate the number correctly classified
+        pred = apply(a2, 1, which.max) - 1
+        actual_label = apply(train_data_label, 1, which.max) - 1
+        message("Percentage classified correctly: ",
+                round(sum(actual_label == pred)/n_sample * 100, 4), "%")
+
+        ## c_old = c
+        i = i + 1
+
+        ## Back propagation
+        ##
+        dc_da2 = cost_delta(train_data_label, a2)
+        da2_dt2 = activation_delta(t2)
+        dt2_dw = t(translation_delta_weight(a1))
+        dt2_da1 = translation_delta_x(w1)
+        da1_dt1 = activation_delta(t1)
+        dt1_dw = t(translation_delta_weight(train_data))
+
+        w0 = w0 -
+            gamma * t(t(((dc_da2 * da2_dt2) %*% dt2_da1) * da1_dt1) %*% dt1_dw)
+        w1 = w1 - gamma * t(t(dc_da2 * da2_dt2) %*% dt2_dw)
+
+    }
+}
--- mnist/mnist_train.R
+++ mnist/mnist_train.R
@@ -2,8 +2,9 @@ source("mnist_loader.R")
 source("fnn.R")
 
 ## Subset the data
-train_data = train_data[1:100, ]
-train_data_label = train_data_label[1:100]
+n = 10000
+train_data = train_data[1:n, ]
+train_data_label = train_data_label[1:n]
 
 ## Include intercept
 train_data = cbind(1, train_data)
@@ -20,59 +21,17 @@ ind = matrix(c(1:length(train_data_label),
 train_data_label_bin[ind] = 1
 
 ## Initialisation
-n = nrow(train_data)
 input_size = ncol(train_data)
 hidden_size = 30
 output_size = 10
 gamma = 1e-3
 maxIter = 1000
 tol = 1e-10
+w0 = initialise_weight(input_size, hidden_size)
+w1 = initialise_weight(hidden_size, output_size)
 
-w0 = apply(X = matrix(rnorm(input_size * hidden_size),
-                      nr = input_size,
-                      nc = hidden_size),
-           MARGIN = 2,
-           FUN = function(x) x/sum(x))
-w1 = apply(X = matrix(rnorm(hidden_size * output_size),
-                      nr = hidden_size,
-                      nc = output_size),
-           MARGIN = 2,
-           FUN = function(x) x/sum(x))
+model = train(data = train_data, label = train_data_label_bin,
+              weights = list(w0, w1), maxIter = 10000, tol = 1e-03,
+              gamma = 1e-3, sampling_pct = 0.1)
 
-i = 1
-c_old = -Inf
-while(i <= maxIter){
-    ## Forward propagation
-    t1 = translation(train_data, w0)
-    a1 = activation(t1)
-    t2 = translation(a1, w1)
-    a2 = activation(t2)
 
-    ## print(head(a2))
-    ## print(head(train_data_label_bin))
-
-    c = cost(train_data_label_bin, a2)
-    message("Cost: ", c)
-    ## if((c - c_old) < tol)
-    ##     break
-
-    ## Calculate the number correctly classified
-    pred = apply(a2, 1, which.max) - 1
-    print(sum(train_data_label == pred)/n)
-
-    ## c_old = c
-    i = i + 1
-
-    ## Back propagation
-    ##
-    dc_da2 = cost_delta(train_data_label_bin, a2)
-    da2_dt2 = activation_delta(t2)
-    dt2_dw = t(translation_delta_weight(a1))
-    dt2_da1 = translation_delta_x(w1)
-    da1_dt1 = activation_delta(t1)
-    dt1_dw = t(translation_delta_weight(train_data))
-
-    w0 = w0 - gamma * t(t(((dc_da2 * da2_dt2) %*% dt2_da1) * da1_dt1) %*% dt1_dw)
-    w1 = w1 - gamma * t(t(dc_da2 * da2_dt2) %*% dt2_dw)
-
-}
