0b4526e110f99bc793905790a6b69c38279f68a4
mk <mkao006@gmail.com>
2016-07-27 21:08:57 -0700

    renamed badly named functions and variables, generalised the function and also added the prediction function

--- mnist/fnn/R/fnn.R
+++ mnist/fnn/R/fnn.R
@@ -1,4 +1,4 @@
-cost = function(p, q){
+cross_entropy = function(p, q){
     ## NOTE (Michael): This is to avoid the cross-entropy to be undefined when q
     ##                 is 1 or 0. However, there should be a better way of
     ##                 dealing with this.
@@ -8,7 +8,7 @@ cost = function(p, q){
     -sum(s) * (1/NROW(q))
 }
 
-activation = function(x){
+sigmoid = function(x){
     1/(1 + exp(-x))
 }
 
@@ -16,13 +16,13 @@ translation = function(x, w){
     x %*% w
 }
 
-cost_delta = function(p, q){
+cross_entropy_delta = function(p, q){
     k = 1e-15
     sq = ifelse(q == 0, k, ifelse(q == 1, 1 - k, q))
     (p - sq)/(sq * (sq - 1))
 }
 
-activation_delta = function(x){
+sigmoid_delta = function(x){
     exp(x)/((1 + exp(x))^2)
 }
 
@@ -34,102 +34,151 @@ translation_delta_x = function(w){
     t(w)
 }
 
-initialise_weight = function(input_size, output_size){
-    apply(X = matrix(rnorm(input_size * output_size),
-                     nr = input_size,
-                     nc = output_size),
-          MARGIN = 2,
-          FUN = function(x) x/sum(x))
+initialise_weights = function(size){
+    layers = length(size) - 1
+    weights = vector(mode = "list", layers)
+    for(layer in 1:layers){
+        weights[[layer]] =
+            apply(X = matrix(rnorm(size[layer] * size[layer + 1]),
+                             nr = size[layer],
+                             nc = size[layer + 1]),
+                  MARGIN = 2,
+                  FUN = function(x) x/sum(x))
+    }
+    weights
 }
 
-train = function(data,
-                 label,
-                 weights,
-                 maxIter,
-                 tol,
-                 stochastic = TRUE,
-                 sampling_pct = 0.3,
-                 gamma = 1e-3){
+fp = function(data, weights, activationFUN){
+
+    n.layers = length(weights) + 1
+
+    translation_layer = vector("list", n.layers)
+    translation_layer[[1]] = data
+    activation_layer = translation_layer
+
+
+    for(layer in 1:(n.layers - 1)){
+        translation_layer[[layer + 1]] =
+            translation(translation_layer[[layer]], weights[[layer]])
+        activation_layer[[layer + 1]] =
+            activationFUN(translation_layer[[layer + 1]])
+    }
+    list(translation_layer = translation_layer,
+         activation_layer = activation_layer)
+}
+
+
+
+fnn = function(data,
+               label,
+               size,
+               costFUN,
+               activationFUN,
+               costDerivFUN,
+               activationDerivFUN,
+               maxIter,
+               tol,
+               stochastic = TRUE,
+               sampling_pct = 0.3,
+               gamma = 1e-3){
+
+    ## Check
+    if(ncol(data) != size[1])
+        stop("Incorrect input size")
+    if(ncol(label) != size[length(size)])
+        stop("Incorrect output size")
+
+    ## Initialisation
+    n.sample= size[1]
+    n.layers = length(size)
+    weights = initialise_weights(size)
     i = 1
-    c_old = Inf
 
-    n = NROW(data)
-    size = c(ncol(data), ncol(weights[[1]]), ncol(weights[[2]]))
+    dt_dw = vector("list", n.layers - 1)
+    dt_da = vector("list", n.layers - 2)
+    da_dt = vector("list", n.layers - 1)
 
+    ## Start stochastic gradient descent
     while(i <= maxIter){
         if(stochastic){
-            n_sample = floor(n * sampling_pct)
-            index_sample = sample(n, n_sample)
+            n_sample = floor(n.sample * sampling_pct)
+            index_sample = sample(n.sample, n_sample)
             train_data = data[index_sample, ]
             train_data_label = label[index_sample, ]
         } else {
-            n_sample = n
+            n_sample = n.sample
             train_data = data
             train_data_label = label
         }
 
-        ## Initialise t and a, a has same dimension as t
-        t = list(train_data,
-                 matrix(0, nc = size[2], nr = size[1]),
-                 matrix(0, nc = size[3], nr = size[2]))
-        a = t
-
-        dc_da = matrix(0, nr = size[1], nc = size[3])
-        da_dt = list(matrix(0, nr = size[1], nc = size[2]),
-                     matrix(0, nr = size[1], nc = size[3]))
-        dt_dw = list(matrix(0, nr = size[1], nc = size[2]),
-                     matrix(0, nr = size[1], nc = size[3]))
-        dt_da = list(matrix(0, nr = size[2], nc = size[1]),
-                     diag(1, nr = size[3], nc = size[3]))
-
-
         ## Forward propagation
-        for(layer in 1:length(weights)){
-            t[[layer + 1]] = translation(t[[layer]], weights[[layer]])
-            a[[layer + 1]] = activation(t[[layer + 1]])
-        }
+        forward = fp(data = train_data,
+                     weights = weights,
+                     activationFUN = activationFUN)
 
-        c = cost(train_data_label, a[[3]])
-        message("Cost: ", c)
+        ## Compute the cost
+        cost = costFUN(train_data_label, forward$activation_layer[[n.layers]])
+        message("Cost: ", cost)
 
-        ## NOTE (Michael): We need to use the absolute value as we are using
-        ##                 stochastic gradient descent. Need to implement better
-        ##                 convergence criteria.
-        if(abs(c - c_old) < tol)
-            break
+        ## TODO (Michael): Need a way to identify the convergence of the
+        ##                 stochastic gradient descent.
 
         ## Calculate the number correctly classified
-        pred = apply(a[[3]], 1, which.max) - 1
+        pred = apply(forward$activation_layer[[n.layers]], 1, which.max) - 1
         actual_label = apply(train_data_label, 1, which.max) - 1
         message("Percentage classified correctly: ",
                 round(sum(actual_label == pred)/n_sample * 100, 4), "%")
 
-        c_old = c
+        ## Increment i
         i = i + 1
 
         ## Back propagation
-        ##
-
-        dc_da = cost_delta(train_data_label, a[[length(a)]])
-
         w_tmp = weights
-        ## dc_dt = t(dc_da * da_dt[[2]])
-        ## dt_da[[3]] = dc_da
-        ## dt_dt = 1
-        dc_dt = dc_da
 
-        for(layer in length(weights):1){
-            da_dt[[layer]] = activation_delta(t[[layer + 1]])
-            dt_dw[[layer]] = t(translation_delta_weight(a[[layer]]))
-            if(layer <= (length(weights) - 1))
-                dt_da[[layer]] = translation_delta_x(weights[[layer + 1]])
+        dc_da =
+            costDerivFUN(train_data_label, forward$activation_layer[[n.layers]])
+        da_dt[[n.layers - 1]] =
+            activationDerivFUN(forward$translation_layer[[n.layers]])
+        dc_dt = dc_da * da_dt[[n.layers - 1]]
+        dt_dw[[n.layers - 1]] =
+            t(translation_delta_weight(forward$activation_layer[[n.layers - 1]]))
+        w_tmp[[n.layers - 1]] =
+            w_tmp[[n.layers - 1]] - gamma * t(t(dc_dt) %*% dt_dw[[n.layers - 1]])
+
+        for(layer in (n.layers - 2):1){
+            ## Calculate individual derivatives
+            da_dt[[layer]] =
+                activationDerivFUN(forward$translation_layer[[layer + 1]])
+            dt_dw[[layer]] =
+                t(translation_delta_weight(forward$activation_layer[[layer]]))
+            dt_da[[layer]] =
+                translation_delta_x(weights[[layer + 1]])
+
+            ## Update derivative of cost to each layer
             dc_dt = (dc_dt %*% dt_da[[layer]]) * da_dt[[layer]]
+
+            ## Update the weights
             w_tmp[[layer]] =
                 w_tmp[[layer]] - gamma * t(t(dc_dt) %*% dt_dw[[layer]])
         }
         weights = w_tmp
     }
+
+    ## Return model
+    model = list(data = data,
+                 label = label,
+                 size = size,
+                 costFUN = costFUN,
+                 activationFUN = activationFUN,
+                 weights = weights)
+    class(model) = "fnn"
+    model
 }
 
 predict = function(data, model){
+    n.layers = length(model$size)
+    forward =
+        with(model,
+             fp(data, model$weights, activationFUN))
+    forward$activation_layer[n.layers]
 }
