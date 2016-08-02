05451b32cd0858955c9b6f86101524c9e22bb486
mk <mkao006@gmail.com>
2016-08-02 11:34:51 -0700

    changed the loading path

--- mnist/fnn/python/mnist_train.py
+++ mnist/fnn/python/mnist_train.py
@@ -1,7 +1,7 @@
 import mnist_loader
 import network
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper('mnist.pkl.gz')
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper('/home/mk/Github/deep_learning/mnist/data/mnist.pkl.gz')
 
 
 net = network.Network([784, 30, 10])
