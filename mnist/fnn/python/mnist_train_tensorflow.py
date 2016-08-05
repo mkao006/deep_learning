333213e59ee7dd23b395779b906bf79ec86b7c3d
mk <mkao006@gmail.com>
2016-08-04 20:04:08 -0700

    rewrote the model

--- mnist/fnn/python/mnist_train_tensorflow.py
+++ mnist/fnn/python/mnist_train_tensorflow.py
@@ -1,88 +1,65 @@
-# import mnist_loader
-# import network
-# import numpy as np
-
-# training_data, validation_data, test_data = mnist_loader.load_data_wrapper('/home/mk/Github/deep_learning/mnist/data/mnist.pkl.gz')
-
-## Try to convert the above data to Tensorflow friendly format
-
-## Import the data
-# from tensorflow.examples.tutorials.mnist import input_data
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-## ---------------------------------------------------------------------
-## Initialise and create the graph
-## ---------------------------------------------------------------------
 
-## Initialise input and output
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
+## Initialise parameters
+size = [784, 30, 10]
+gamma = 0.05
+batch_size = 300
+iter = 15001
 
-## Specify the model
-## multi-nomial regression
-# w = tf.Variable(tf.zeros([784, 10]))
-# b = tf.Variable(tf.zeros([10]))
-# hidden1 = tf.matmul(x, w) + b
-# y_hat = tf.nn.softmax(hidden1)
+## Initialise the placeholder for data
+data = tf.placeholder(tf.float32, [None, size[0]])
+label = tf.placeholder(tf.float32, [None, size[2]])
 
-## two layer multi-nomial regression
-w1 = tf.Variable(tf.zeros([784, 15]))
-b1 = tf.Variable(tf.zeros([15]))
-hidden1 = tf.nn.sigmoid(tf.matmul(x, w1) + b1)
-w2 = tf.Variable(tf.zeros([15, 10]))
-b2 = tf.Variable(tf.zeros([10]))
-hidden2 = tf.matmul(hidden1, w2) + b2
-y_hat = tf.nn.softmax(hidden2)
+## Initialise the variables
+def init_weight(shape):
+    return tf.Variable(tf.random_normal(shape, stddev = 0.01))
 
-## Specify the cost function
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_hat),
-                                              reduction_indices=[1]))
+def init_bias(size):
+    return tf.Variable(tf.zeros(size))
 
-## Specify the training algorithm
-train_step = tf.train.GradientDescentOptimizer(0.8).minimize(cross_entropy)
+w_h = init_weight(size[:-1])
+w_o = init_weight(size[1:])
+b_h = init_bias([size[1]])
+b_o = init_bias([size[2]])
 
+## Create the model
+def nnet(data, w_h, w_o, b_h, b_o):
+    hidden1 = tf.nn.sigmoid(tf.matmul(data, w_h) + b_h)
+    return tf.matmul(hidden1, w_o) + b_o
+
+# Create the fitted value
+y_hat = nnet(data, w_h, w_o, b_h, b_o)
+
+## Construct the cost
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_hat, label))
+
+## construct an optimizer
+train_alg = tf.train.GradientDescentOptimizer(gamma).minimize(cost)
 
 ## Speficy the comparison
-correct_prediction = tf.equal(tf.argmax(y_hat, 1), tf.argmax(y, 1))
+correct_prediction = tf.equal(tf.argmax(y_hat, 1), tf.argmax(label, 1))
 
 ## Specify the evaluation
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-## Initialise variables. This means x, w, b and y_hat
+## Initialise variables
 init = tf.initialize_all_variables()
 
-
-## ---------------------------------------------------------------------
-## Create the session and train the model
-## ---------------------------------------------------------------------
-
-## Start the session
-sess = tf.Session()
-sess.run(init)
-
-## NOTE (Michael): Create a grid here to search for batch_size, iteration and
-##                 gamma.
-
-## Start the training
-for i in range(10000):
-  batch_size = 50
-  batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-  sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
-  if(i % 500 == 0):
-    print("batch " + str(i))
-    print(sess.run(accuracy,
-                   feed_dict={x: mnist.train.images, y: mnist.train.labels}))
-
-## ---------------------------------------------------------------------
-## Check predictions
-## ---------------------------------------------------------------------
-
-
-## Make prediction on the test and validation dataset
-print(sess.run(accuracy,
-               feed_dict={x: mnist.test.images, y: mnist.test.labels}))
-
-print(sess.run(accuracy,
-               feed_dict={x: mnist.validation.images, y: mnist.validation.labels}))
+with tf.Session() as sess:
+    sess.run(init)
+    ## Start the iteration
+    for i in xrange(1, iter):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        sess.run(train_alg, feed_dict={data: batch_xs, label: batch_ys})
+        if(i % 1000 == 0):
+            print("batch " + str(i))
+            print(sess.run(accuracy,
+                           feed_dict = {data: mnist.train.images,
+                                        label: mnist.train.labels}))
+            print(sess.run(accuracy,
+                           feed_dict = {data: mnist.test.images,
+                                        label: mnist.test.labels}))
