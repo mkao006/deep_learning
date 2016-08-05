6bd9cdf169d4765e90b727d91a6e2679710550e6
mk <mkao006@gmail.com>
2016-08-05 16:12:15 -0700

    added new model

--- mnist/fnn/python/mnist_train.py
+++ mnist/fnn/python/mnist_train.py
@@ -1,8 +1,14 @@
+## Load the data
 import mnist_loader
-import network
-
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper('/home/mk/Github/deep_learning/mnist/data/mnist.pkl.gz')
 
+size = [784, 30, 10]
+## Model 1
+import fnn
+net = fnn.fnn(size)
+net.train(training_data, 30, 10, 3.0, test_data=test_data)
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+## Model 2
+import network
+network = network.Network(size)
+network.SGD(training_data, 30, 10, 3.0, test_data=test_data)
